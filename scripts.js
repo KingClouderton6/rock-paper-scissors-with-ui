794d79afecfd7f3d83db61d47d6a6a29f98ec84e
KingClouderton6 <islandintheclouds@outlook.com>
2021-11-22 00:44:34 +0000

    Added event listener to all buttons and set them to run playRound() and change playerSelection depending on button clicked; refactored and added if statement logic for round victories. Played around with colours

--- scripts.js
+++ scripts.js
@@ -1,9 +1,68 @@
 const rockButton = document.querySelector('#Rock');
 const paperButton = document.querySelector('#Paper');
 const scissorsButton = document.querySelector('#Scissors');
+
+let playerSelection;
+let computerSelection;
+
 const choice = ['Rock', 'Paper', 'Scissors'];
 
+function computerDecision (){
+    computerSelection = choice[Math.floor(Math.random() * choice.length)];
+    return computerSelection;
+}
 
-const computerChoice = () => choice[Math.floor(Math.random() * choice.length)];  
-const computerSelection = computerChoice();
 
+rockButton.addEventListener('click', () => {
+    playerSelection = "rock";
+    playRound(playerSelection, computerSelection)
+    return playerSelection;
+});
+
+paperButton.addEventListener('click', () => {
+    playerSelection = "paper";
+    playRound(playerSelection, computerSelection)
+    return playerSelection;
+});
+
+scissorsButton.addEventListener('click', () => {
+    playerSelection = "scissors";
+    playRound(playerSelection, computerSelection)
+    return playerSelection;
+});
+
+
+function playRound(playerSelection, computerSelection) {
+    computerDecision();
+
+   if (playerSelection === "rock") {
+       if (computerSelection === "Rock") {
+        console.log("You draw!");
+       } else if (computerSelection === "Scissors") {
+        console.log("You lose!");
+       } else {
+           console.log("You win!");
+       } 
+   } 
+
+   if (playerSelection === "scissors") {
+    if (computerSelection === "Rock") {
+        console.log("You lose!");
+    } else if (computerSelection === "Paper") {
+        console.log("You win!");
+    } else {
+        console.log("You draw.");
+    }
+   }
+
+   if (playerSelection === "paper") {
+    if (computerSelection === "Rock") {
+        console.log("You win!");
+    } else if (computerSelection === "Paper") {
+        console.log("You draw!");
+    } else {
+        console.log("You lose.");
+    }
+   }
+
+}
